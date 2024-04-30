3296decb6797b95a38bf3eb3a7975113c8524f11
usman.naeem <usmaanbinnaeem@gmail.com>
2024-05-01 02:29:46 +0500

    Improvements: Created Garlic Page

--- src/pages/garlic.tsx
+++ src/pages/garlic.tsx
@@ -1,19 +1,26 @@
-import * as React from "react";
-import type { HeadFC, PageProps } from "gatsby";
-import Hero from "../components/Recipes/Hero";
-import Video from "../components/Garlic/VideoSection";
-import Ratio from "../components/Garlic/Ratio";
-import Description from "../components/Garlic/Description";
+import type { HeadFC, PageProps } from 'gatsby';
+import * as React from 'react';
+import Description from '../components/Garlic/Description';
+import Ratio from '../components/Garlic/Ratio';
+import Video from '../components/Garlic/VideoSection';
+import Hero from '../components/Recipes/Hero';
+import Footer from '../components/Footer';
 
 const GarlicPage: React.FC<PageProps> = () => {
-    return (
-        <main>
-            <Hero title={"40 Clove Garlic Chicken"} discription={"There's no such thing as 'too much garlic' withour 40 Clove Garlic Chicken."} />
-            <Video />
-            <Ratio />
-            <Description />
-        </main>
-    );
+  return (
+    <main>
+      <Hero
+        title={'40 Clove Garlic Chicken'}
+        discription={
+          "There's no such thing as 'too much garlic' withour 40 Clove Garlic Chicken."
+        }
+      />
+      <Video />
+      <Ratio />
+      <Description />
+      <Footer />
+    </main>
+  );
 };
 
 export default GarlicPage;
