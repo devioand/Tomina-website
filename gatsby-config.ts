8d72395dbc5b52cc40cb082fe5a083f27821a043
Asad Javed <devioand.aj@gmail.com>
2024-05-05 14:14:59 +0500

    Enhancements: Added favicon icon

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,23 +3,35 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Tomina`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-postcss", {
-    resolve: `gatsby-source-filesystem`,
-    options: {
-      name: `images`,
-      path: `${__dirname}/src/images`,
-      // Ignore files starting with a dot
-      // ignore: [`**/\.*`],
-      // Use "mtime" and "inode" to fingerprint files (to check if file has changed)
-      // fastHash: true,
+  plugins: [
+    "gatsby-plugin-postcss",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+        // Ignore files starting with a dot
+        // ignore: [`**/\.*`],
+        // Use "mtime" and "inode" to fingerprint files (to check if file has changed)
+        // fastHash: true,
+      },
     },
-  },]
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Tomino`,
+        short_name: `Tomino`,
+        start_url: `/`,
+        icon: `src/images/favicon.ico`, // This path is relative to the root of the site.
+      },
+    },
+  ],
 };
 
 export default config;
