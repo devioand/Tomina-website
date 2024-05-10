43a489a7498dd79fab83a268f273bd8ee1203600
Asad Javed <devioand.aj@gmail.com>
2024-05-10 22:53:24 +0500

    Installed static image of Gatsby

--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -5,18 +5,28 @@ import Hero from "../components/product/Hero";
 import Product from "../components/product/Products";
 import CustomersTestimonials from "../components/product/CustomersTestimonials";
 import PlayButton from "../components/svgs/solid/PlayButton";
+import { StaticImage } from "gatsby-plugin-image";
 
 const ProductPage: React.FC<PageProps> = () => {
   return (
     <main>
       <Hero />
       <Product />
-      <div className="relative bg-size bg-[url('../images/imgs/products/garlicPowder.webp')] h-[50vh] md:h-[100vh] md:-mt-[108px]">
+      <div className="relative">
+        <StaticImage
+          src="../images/imgs/products/garlicPowder.webp"
+          alt="bottles"
+          layout="fullWidth"
+        />
         <span className="absolute top-[42%] md:top-[52%] left-[47%]">
           <PlayButton />
         </span>
       </div>
-      <div className="bg-size bg-[url('../images/imgs/products/our-mission.jpg')] h-[50vh] md:h-[118vh] md:-mt-[120px]"></div>
+      <StaticImage
+        src="../images/imgs/products/our-mission.jpg"
+        alt="Showcase"
+        placeholder="blurred"
+      />
       <CustomersTestimonials />
       <Footer curveColor="#282828" />
     </main>
